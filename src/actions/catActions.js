597df42040c74a8d13600a2afa948870bc86b01e
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-26 00:34:23 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -6,12 +6,4 @@ export const fetchCats = () => {
       .then(cats => dispatch({ type: 'ADD_CATS', cats: cats.images})
     )
   }
-}// write and export your action creator function hereexport const fetchCats = () => {
-  return (dispatch) => {
-    dispatch({ type: 'LOADING_CATS' })
-    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
-      .then(response => response.json())
-      .then(cats => dispatch({ type: 'ADD_CATS', cats: cats.images})
-    )
-  }
 }
